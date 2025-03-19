86e0d757c3b33404833a4f374c6af77e23286616
GooeyAvocado <the.gooey.avocado@gmail.com>
2025-03-19 19:46:57 -0400

    Reusable Context Menu

---
+++ chamomile.web/src/components/shared/ContextMenu.tsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { Divider, ListItemIcon } from '@mui/material';
+
+export class ContextMenuOption {
+    text?: React.ReactNode = "";
+    customContent?:(onClose:()=>void) => React.ReactNode
+    onClick?: () => void = ()=>{};
+    type?: "item"|"divider"|"custom" = "item"
+    icon?: React.ReactNode;
+}
+
+
+export default function ContextMenu(props:{
+    children:React.ReactNode
+    options: ContextMenuOption[]
+}) {
+  const [contextMenu, setContextMenu] = React.useState<{
+    mouseX: number;
+    mouseY: number;
+  } | null>(null);
+
+  const handleContextMenu = (event: React.MouseEvent) => {
+    event.preventDefault();
+    setContextMenu(
+      contextMenu === null
+        ? {
+            mouseX: event.clientX + 2,
+            mouseY: event.clientY - 6,
+          }
+        : // repeated contextmenu when it is already open closes it with Chrome 84 on Ubuntu
+          // Other native context menus might behave different.
+          // With this behavior we prevent contextmenu from the backdrop to re-locale existing context menus.
+          null,
+    );
+  };
+
+  const handleClose = () => {
+    setContextMenu(null);
+  };
+
+  return (
+    <div onContextMenu={handleContextMenu} style={{ cursor: 'context-menu' }}>
+      {props.children}
+      
+      <Menu
+        open={contextMenu !== null}
+        onClose={handleClose}
+        anchorReference="anchorPosition"
+        anchorPosition={
+          contextMenu !== null
+            ? { top: contextMenu.mouseY, left: contextMenu.mouseX }
+            : undefined
+        }
+      >
+        {props.options.map(a=> a.type==="custom"? a.customContent?.(handleClose) : a.type==="divider" ? <Divider/> :
+            <MenuItem onClick={()=>{
+                handleClose();
+                a.onClick?.();
+            }}>
+                {a.icon && <ListItemIcon>{a.icon}</ListItemIcon>} 
+                {a.text}
+            </MenuItem>
+        )}
+      </Menu>
+    </div>
+  );
+}
