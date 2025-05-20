b4b8c2c5b8e3d9935033c644a4e7901ed395b279
GooeyAvocado <the.gooey.avocado@gmail.com>
2025-05-19 20:18:46 -0400

    Narrowable Chamomile logo

--- chamomile.web/src/components/shared/ChamomileLogo.tsx
+++ chamomile.web/src/components/shared/ChamomileLogo.tsx
@@ -1,8 +1,12 @@
-export default function ChamomileLogo() {
+export default function ChamomileLogo(props:{
+    hideWords?: boolean
+}) {
     return <div style={{ display: "flex", alignItems: 'flex-end' }}>
         <img src="color.png" width={"64"} />
-        <div style={{ marginBottom: "-2px", fontSize: "1.7em", fontFamily: "Merriweather" }}>
+        {!props.hideWords && <div style={{ 
+            marginBottom: "-2px", fontSize: "1.7em", fontFamily: "Merriweather" 
+        }}>
             Chamomile
-        </div>
+        </div>}
     </div>
 }
