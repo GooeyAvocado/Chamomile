21d9bc4b7dc0aa31a6b7ec738b821eabbc5b8701
GooeyAvocado <the.gooey.avocado@gmail.com>
2025-05-19 20:21:33 -0400

    Visual improvements to Sampler/Scheduler

--- chamomile.web/src/components/shared/prompt/SamplerSelector.tsx
+++ chamomile.web/src/components/shared/prompt/SamplerSelector.tsx
@@ -1,37 +1,35 @@
-import { FormControl, InputAdornment, InputLabel, MenuItem, Select } from "@mui/material";
+import { FormControl, InputAdornment, MenuItem, Select } from "@mui/material";
 import { getSamplers } from "../../../api/Prompts";
 import useApi from "../../hooks/useApi";
 import { Window } from "@mui/icons-material";
 
-export default function SamplerSelector(props:{
+export default function SamplerSelector(props: {
     sampler: string,
-    setSampler: (val:string) => void
-}){
+    setSampler: (val: string) => void
+}) {
 
-    const {sampler,setSampler} = props
-    const {data} = useApi(getSamplers,true)
+    const { sampler, setSampler } = props
+    const { data } = useApi(getSamplers, true)
 
     return <FormControl fullWidth>
-        <InputLabel>Sampler</InputLabel>
         <Select
             disabled={!data}
             value={sampler ?? ""}
-            label="Sampler"
             onChange={(e) => setSampler(e.target.value)}
             startAdornment={
                 <InputAdornment position="start">
-                    <Window/>
+                    <Window />
                 </InputAdornment>
             }
             endAdornment={
                 <InputAdornment position="end">
-                    <div style={{marginRight:"15px"}}>Sampler</div>
+                    <div style={{ marginRight: "15px" }}>Sampler</div>
                 </InputAdornment>
             }
         >
-            {data?.map(a=><MenuItem value={a.name}>{a.name}</MenuItem>)}
+            {data?.map(a => <MenuItem value={a.name}>{a.name}</MenuItem>)}
         </Select>
     </FormControl>
-    
+
 
 }
--- chamomile.web/src/components/shared/prompt/SchedulerSelector.tsx
+++ chamomile.web/src/components/shared/prompt/SchedulerSelector.tsx
@@ -1,32 +1,30 @@
-import { FormControl, InputAdornment, InputLabel, MenuItem, Select } from "@mui/material";
+import { FormControl, InputAdornment, MenuItem, Select } from "@mui/material";
 import { getSchedulers } from "../../../api/Prompts";
 import useApi from "../../hooks/useApi";
 import { Schedule } from "@mui/icons-material";
 
-export default function SchedulerSelector(props:{
+export default function SchedulerSelector(props: {
     scheduler: string,
-    setScheduler: (val:string) => void
-}){
+    setScheduler: (val: string) => void
+}) {
 
-    const {scheduler,setScheduler} = props
-    const {data} = useApi(getSchedulers,true)
+    const { scheduler, setScheduler } = props
+    const { data } = useApi(getSchedulers, true)
 
     return <FormControl fullWidth>
-        <InputLabel>Scheduler</InputLabel>
         <Select
             disabled={!data}
             value={scheduler ?? ""}
-            label="Scheduler"
             onChange={(e) => setScheduler(e.target.value)}
             startAdornment={
                 <InputAdornment position="start">
-                    <Schedule/>
+                    <Schedule />
                 </InputAdornment>
             }
         >
-            {data?.map(a=><MenuItem value={a.label}>{a.label}</MenuItem>)}
+            {data?.map(a => <MenuItem value={a.label}>{a.label}</MenuItem>)}
         </Select>
     </FormControl>
-    
+
 
 }
