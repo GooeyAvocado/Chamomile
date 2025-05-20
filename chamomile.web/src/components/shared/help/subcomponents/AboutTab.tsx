0d0d61f0e7b88ba4a38361a0d86ebb23d3499587
GooeyAvocado <the.gooey.avocado@gmail.com>
2025-05-19 20:45:38 -0400

    CivitAI Post link

--- chamomile.web/src/components/shared/help/subcomponents/AboutTab.tsx
+++ chamomile.web/src/components/shared/help/subcomponents/AboutTab.tsx
@@ -1,6 +1,6 @@
 import { GitHub } from "@mui/icons-material";
 import ChamomileLogo from "../../ChamomileLogo";
-import { Link } from "@mui/material";
+import { Link, Tooltip } from "@mui/material";
 
 export default function AboutTab() {
     return <>
@@ -11,7 +11,12 @@ export default function AboutTab() {
         <hr style={{ width: "50%", minWidth: '300px' }} />
         <img src="/images/googgos.png" style={{ width: "50%", minWidth: "300px" }} />
         <hr style={{ width: "50%", minWidth: '300px' }} />
-        <div style={{ marginBottom: '20px' }}>See more of the Googgos on CivitAI</div>
+        <div style={{ marginBottom: '20px' }}>
+            <Link color="info" href="https://civitai.green/posts/17173953"> See more of the <Tooltip title="Goo Doggos">
+                <u style={{ textDecorationStyle: "dashed" }}>
+                    Googgos
+                </u></Tooltip> on CivitAI</Link>
+        </div>
         <div style={{ marginTop: "10px", display: "flex", flexDirection: 'column', gap: "10px" }}>
             <div style={{ display: 'flex', gap: "10px" }}>
                 <GitHub />
