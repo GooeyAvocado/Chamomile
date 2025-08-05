327638900ec753a4f79270497dbaf0d075ae2a28
GooeyAvocado <the.gooey.avocado@gmail.com>
2025-08-05 10:55:33 -0400

    Better welcome pane

--- chamomile.web/src/components/shared/ChamomileLogo.tsx
+++ chamomile.web/src/components/shared/ChamomileLogo.tsx
@@ -1,8 +1,15 @@
 export default function ChamomileLogo(props: {
     hideWords?: boolean
     wordsOverride?: string
+    vertical?: boolean
 }) {
-    return <div style={{ display: "flex", alignItems: 'flex-end' }}>
+
+    const additionalStyle = props.vertical ? {
+        flexDirection: 'column',
+        alignItems: 'center'
+    } as React.CSSProperties : {}
+
+    return <div style={{ display: "flex", alignItems: 'flex-end', ...additionalStyle }}>
         <img src="color.png" width={"64"} />
         {!props.hideWords && <div style={{
             marginBottom: "-2px", fontSize: "1.7em", fontFamily: "Merriweather"
--- chamomile.web/src/components/shared/welcome/WelcomePane.tsx
+++ chamomile.web/src/components/shared/welcome/WelcomePane.tsx
@@ -1,12 +1,11 @@
 import ChamomileLogo from "../ChamomileLogo";
 
-export default function WelcomePane(){
-    return <div style={{height:"100%", display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
-        <ChamomileLogo/>
-        <hr style={{maxWidth:"100%", width:'500px'}}/>
-        <div style={{fontFamily:'Merriweather'}}>Welcome to Camomile!</div>
-        <div style={{fontSize:'.8em'}}>
-            Brew an image to get started
+export default function WelcomePane() {
+    return <div style={{ height: "100%", display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <ChamomileLogo wordsOverride="Welcome to Chamomile" vertical />
+        <hr style={{ maxWidth: "100%", width: '500px' }} />
+        <div style={{ fontSize: '.8em' }}>
+            Brew an image to get started, or review the help section if you're unable to brew
         </div>
     </div>
 }
