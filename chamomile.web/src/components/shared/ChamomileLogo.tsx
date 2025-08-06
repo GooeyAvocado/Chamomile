958ababeb3d65be03e0558def16efd0f7fa762b2
GooeyAvocado <the.gooey.avocado@gmail.com>
2025-08-05 22:23:21 -0400

    Small Chamomile logo

--- chamomile.web/src/components/shared/ChamomileLogo.tsx
+++ chamomile.web/src/components/shared/ChamomileLogo.tsx
@@ -2,6 +2,7 @@ export default function ChamomileLogo(props: {
     hideWords?: boolean
     wordsOverride?: string
     vertical?: boolean
+    small?: boolean
 }) {
 
     const additionalStyle = props.vertical ? {
@@ -10,9 +11,9 @@ export default function ChamomileLogo(props: {
     } as React.CSSProperties : {}
 
     return <div style={{ display: "flex", alignItems: 'flex-end', ...additionalStyle }}>
-        <img src="color.png" width={"64"} />
+        <img src="color.png" width={props.small ? "40" : "64"} />
         {!props.hideWords && <div style={{
-            marginBottom: "-2px", fontSize: "1.7em", fontFamily: "Merriweather"
+            marginBottom: "-2px", fontSize: props.small ? "1.2em" : "1.7em", fontFamily: "Merriweather"
         }}>
             {props.wordsOverride ?? "Chamomile"}
         </div>}
