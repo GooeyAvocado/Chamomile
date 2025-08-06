813065dc277ed6451387dc54845a616b175196fc
GooeyAvocado <the.gooey.avocado@gmail.com>
2025-08-05 22:22:52 -0400

    New Navbar

--- chamomile.web/src/components/shared/navbar/Navbar.tsx
+++ chamomile.web/src/components/shared/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { Divider, IconButton, ListItemIcon, Menu, MenuItem } from "@mui/material
 import { useWindowDimensions } from "../../hooks/useWindowDimensions";
 import ChamomileLogo from "../ChamomileLogo";
 import StatusButton from "../StatusButton/StatusButton";
-import { Coffee, Menu as MenuIcon, PhotoLibrary, Settings } from "@mui/icons-material";
+import { Coffee, Launch, Menu as MenuIcon, Monitor, PhotoLibrary, Settings } from "@mui/icons-material";
 import { useState } from "react";
 import SettingsSlidein from "../settings/SettingsSlidein";
 import HelpButton from "../help/HelpButton";
@@ -26,12 +26,23 @@ export default function Navbar({ navBrew, navAlbums }: {
         setPromptAnchor(null)
     }
 
+    const onDisplay = () => {
+        window.open('/#/display', '_blank');
+    };
+
+
+
     return <>
-        <div style={{ display: 'flex', justifyContent: "space-between", width: "100%", alignItems: "end" }}>
+        <div style={{
+            display: 'flex', justifyContent: "space-between", width: "100%", alignItems: "flex-end", background: "#252525",
+            paddingLeft: "20px", paddingRight: "20px", paddingTop: "10px", paddingBottom: "20px"
+
+        }}>
             <div onClick={navBrew} style={{ cursor: 'pointer' }}>
-                <ChamomileLogo hideWords={width < 450} />
+                <ChamomileLogo hideWords={width < 450} small />
             </div>
-            <div style={{ display: 'flex', gap: "10px" }}>
+            {/* This translation is for visual purposes. The steam of the chamomile cup makes it look like this is out of alignment */}
+            <div style={{ display: 'flex', gap: "10px", transform: "translateY(7px)" }}>
                 <StatusButton />
                 <HelpButton />
                 <IconButton onClick={(e) => setPromptAnchor(e.currentTarget)} >
@@ -57,6 +68,11 @@ export default function Navbar({ navBrew, navAlbums }: {
                 <ListItemIcon><PhotoLibrary fontSize="small" /></ListItemIcon>
                 Collections
             </MenuItem>
+            <MenuItem onClick={() => { onClose(); onDisplay(); }} style={{ fontSize: ".8em" }}>
+                <ListItemIcon><Monitor fontSize="small" /></ListItemIcon>
+                Display
+                <Launch fontSize="small" style={{ marginLeft: "2px", scale: ".8" }} />
+            </MenuItem>
 
             <Divider />
 
@@ -70,8 +86,6 @@ export default function Navbar({ navBrew, navAlbums }: {
         </Menu>
 
         <SettingsSlidein open={settingsOpen} setOpen={setSettingsOpen} />
-
-        <hr style={{ width: "100%" }} />
 
     </>
 }
