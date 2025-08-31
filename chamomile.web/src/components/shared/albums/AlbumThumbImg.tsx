1eb2ea33d2fa0b6a9211cf8a2d67e4ab7d1c07fd
GooeyAvocado <the.gooey.avocado@gmail.com>
2025-08-30 23:36:33 -0400

    Fix AlbumThumbImg

--- chamomile.web/src/components/shared/albums/AlbumThumbImg.tsx
+++ chamomile.web/src/components/shared/albums/AlbumThumbImg.tsx
@@ -13,9 +13,11 @@ export default function AlbumThumbImg({ album, defaultImage }: {
             album.thumbId
                 ? <img src={imageUrl(album.thumbId)} style={{ width: "100%", height: "100%", aspectRatio: "2/1", objectFit: 'cover', objectPosition: 'center top', position: 'absolute', left: '0', top: '0' }} />
                 : <div style={{ display: 'flex', flexWrap: "wrap", width: "100%", height: "100%", aspectRatio: "2/1", objectFit: 'cover', objectPosition: 'center top', position: 'absolute', left: '0', top: '0' }}>
-                    {album.firstFourImages?.map((a) =>
-                        <img key={`albumsample-${a}`} src={imageUrl(a)} style={{ flex: '1', width: "100%", height: (album.firstFourImages?.length ?? 0) < 3 ? "100%" : "50%", aspectRatio: "2/1", objectFit: "cover", objectPosition: 'center top' }} />
-                    )}
+                    {(album.firstFourImages?.length ?? 0) < 3
+                        ? <img src={imageUrl(album.firstFourImages?.[0])} style={{ width: "100%", height: "100%", aspectRatio: "2/1", objectFit: 'cover', objectPosition: 'center top', position: 'absolute', left: '0', top: '0' }} />
+                        : album.firstFourImages?.map((a) =>
+                            <img key={`albumsample-${a}`} src={imageUrl(a)} style={{ flex: '1', width: "100%", height: "50%", aspectRatio: "2/1", objectFit: "cover", objectPosition: 'center top' }} />
+                        )}
                 </div>
         }
     </div>
