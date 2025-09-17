d401abec9dd32da037556c231edc42fb079a03f8
GooeyAvocado <the.gooey.avocado@gmail.com>
2025-09-17 07:43:06 -0400

    hard refresh on MOdels and LoRAs

--- chamomile.web/src/components/contexts/LoraContext.tsx
+++ chamomile.web/src/components/contexts/LoraContext.tsx
@@ -1,11 +1,11 @@
 import { createContext, useEffect, useState } from "react";
 import useApi from "../hooks/useApi";
 import { Lora } from "../../model/Lora";
-import { getLoras } from "../../api/Loras";
+import { getLoras, refreshLoras } from "../../api/Loras";
 
 export class LoraContextType {
     public constructor(
-        public refresh: () => void,
+        public refresh: (hard?: boolean) => void,
         public loras: Lora[],
         public loading: boolean,
     ) { }
@@ -16,15 +16,22 @@ export const LoraContext = createContext<LoraContextType | undefined>(undefined)
 export const LoraProvider = (props: { children: any }) => {
 
     const lorasApi = useApi(getLoras, true);
-    const refresh = () => { lorasApi.fetch() }
-
+    const hardRefreshApi = useApi(refreshLoras);
     const [loras, setLoras] = useState<Lora[]>([])
+
+    const refresh = (hard?: boolean) => {
+        if (hard) {
+            hardRefreshApi.fetch((data) => { if (data) setLoras(data ?? []) })
+        } else {
+            lorasApi.fetch()
+        }
+    }
 
     useEffect(() => {
         if (lorasApi.data) setLoras(lorasApi.data)
     }, [lorasApi.data])
 
-    return <LoraContext.Provider value={{ loading: lorasApi.loading, loras: loras, refresh: refresh }}>
+    return <LoraContext.Provider value={{ loading: lorasApi.loading || hardRefreshApi.loading, loras: loras, refresh: refresh }}>
         {props.children}
     </LoraContext.Provider>
 
--- chamomile.web/src/components/contexts/ModelsContext.tsx
+++ chamomile.web/src/components/contexts/ModelsContext.tsx
@@ -1,11 +1,11 @@
 import { createContext, useEffect, useState } from "react";
 import useApi from "../hooks/useApi";
 import { Model } from "../../model/Model";
-import { getModels } from "../../api/Model";
+import { getModels, refreshModels } from "../../api/Model";
 
 export class ModelContextType {
     public constructor(
-        public refresh: () => void,
+        public refresh: (hard?: boolean) => void,
         public models: Model[],
         public loading: boolean,
     ) { }
@@ -16,15 +16,22 @@ export const ModelContext = createContext<ModelContextType | undefined>(undefine
 export const ModelProvider = (props: { children: any }) => {
 
     const modelsApi = useApi(getModels, true);
-    const refresh = () => { modelsApi.fetch() }
-
+    const hardRefreshApi = useApi(refreshModels);
     const [models, setModels] = useState<Model[]>([])
+
+    const refresh = (hard?: boolean) => {
+        if (hard) {
+            hardRefreshApi.fetch((data) => { if (data) setModels(data ?? []) })
+        } else {
+            modelsApi.fetch()
+        }
+    }
 
     useEffect(() => {
         if (modelsApi.data) setModels(modelsApi.data)
     }, [modelsApi.data])
 
-    return <ModelContext.Provider value={{ loading: modelsApi.loading, models: models, refresh: refresh }}>
+    return <ModelContext.Provider value={{ loading: modelsApi.loading || hardRefreshApi.loading, models: models, refresh: refresh }}>
         {props.children}
     </ModelContext.Provider>
 
