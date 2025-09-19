861519f2929226a37d973496aefc56072f84c1a8
GooeyAvocado <the.gooey.avocado@gmail.com>
2025-09-18 22:28:20 -0400

    Improved model selector

--- chamomile.web/src/components/shared/prompt/PromptModelSelectorModal.tsx
+++ chamomile.web/src/components/shared/prompt/PromptModelSelectorModal.tsx
@@ -1,11 +1,11 @@
-import { Alert, AlertTitle, Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, Tooltip } from "@mui/material"
+import { Alert, AlertTitle, Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, Tooltip } from "@mui/material"
 import { usePrompt } from "../../hooks/usePrompt"
 import ModelSelector from "../model/ModelSelector";
 import useApi from "../../hooks/useApi";
 import { currentModel, setModel } from "../../../api/Model";
 import { Model } from "../../../model/Model";
 import { useEffect, useState } from "react";
-import { Add, Close, Schedule } from "@mui/icons-material";
+import { Add, Close, Edit, Schedule } from "@mui/icons-material";
 import LoraCard from "../lora/LoraCard";
 import ModelCard from "../model/ModelCard";
 import LoraSelector from "../lora/LoraSelector";
@@ -27,6 +27,7 @@ export default function PromptModelSelectorModal(props: {
     const { open, setOpen, noBrew, prompt: promptOverride, setPrompt: setPromptOverride } = props
 
     const [addOpen, setAddOpen] = useState(false)
+    const [checkpointEditOpen, setCheckpointEditOpen] = useState(false)
 
     const { prompt: globalPrompt, setPrompt: setGlobalPrompt } = usePrompt();
     const currentModelApi = useApi(currentModel)
@@ -45,6 +46,8 @@ export default function PromptModelSelectorModal(props: {
             if (!noBrew) {
                 currentModelApi.fetch();
                 currentSequenceApi.fetch();
+                setAddOpen(false);
+                setCheckpointEditOpen(false)
             }
         }
     }, [open])
@@ -56,6 +59,7 @@ export default function PromptModelSelectorModal(props: {
     }
 
     const additionalModels = (currentSequenceApi.data ?? []).filter(m => m.modelTitle !== currentModelApi.data?.model);
+    const hasSequence = (currentSequenceApi.data?.length ?? 0) > 0
 
     const onChangeModel = (val: Model) => {
         changeModelApi.fetch(() => {
@@ -69,8 +73,8 @@ export default function PromptModelSelectorModal(props: {
                 }, () => {
                     enqueueSnackbar("Checkpoint sequence could not be cleared", { variant: 'error' })
                 }, [])
-                setOpen(false);
             }
+            setCheckpointEditOpen(false)
         }, () => {
             enqueueSnackbar("Checkpoint could not be changed", { variant: 'error' })
         }, { model: val.title } as ModelRequest)
@@ -84,6 +88,8 @@ export default function PromptModelSelectorModal(props: {
         setPrompt({ ...prompt, positivePrompt: prompt.positivePrompt.replace(new RegExp(`<lora:${alias}:\\d*\\.*\\d*>`), "") });
     }
 
+    const checkpointLoading = currentModelApi.loading || changeModelApi.loading || currentSequenceApi.loading;
+
     return <Dialog open={open} onClose={() => setOpen(false)} fullWidth maxWidth='sm'>
 
         <DialogTitle>Models</DialogTitle>
@@ -91,26 +97,47 @@ export default function PromptModelSelectorModal(props: {
         <DialogContent style={{ display: 'flex', flexDirection: 'column', height: '75vh' }}>
             {!noBrew && pong?.SD
                 ? <>
-                    <Alert severity="warning" style={{ marginBottom: '10px', fontSize: '.7em' }}>
-                        <AlertTitle style={{ fontSize: "1.2em" }}>Changing your primary model will affect all pending images</AlertTitle>
-                        Be careful if executing this while there's images brewing!
-                    </Alert>
-                    <div style={{ marginBottom: '10px', display: 'flex', flexDirection: 'column', gap: '10px' }}>
-                        <ModelCard modelTitle={currentModelApi.data?.model} />
-                        {additionalModels.map(m => <ModelCard key={m.modelTitle} modelTitle={m.modelTitle} />)}
-                    </div>
 
-                    <div style={{ display: 'flex', gap: "10px", alignItems: 'center' }}>
-                        <ModelSelector
-                            model={currentModelApi.data?.model} setModel={onChangeModel}
-                            disabled={changeModelApi.loading} loading={changeModelApi.loading}
-                            style={{ marginTop: '5px', marginBottom: '10px', flex: '1' }}
-                        />
-                        <Tooltip title="Change the checkpoint sequence" style={{ marginTop: "-5px" }}><IconButton
-                            disabled={currentModelApi.loading || currentSequenceApi.loading} onClick={() => {
+                    <div style={{ display: 'flex', alignItems: 'center', width: "100%" }}>
+                        <div style={{ flex: "1" }}><b>Checkpoint</b></div>
+                        <Tooltip title={checkpointEditOpen ? 'Cancel' : 'Change checkpoint'}><IconButton disabled={checkpointLoading} onClick={() => setCheckpointEditOpen(!checkpointEditOpen)}>{checkpointEditOpen ? <Close /> : <Edit />}</IconButton></Tooltip>
+                        <Tooltip title={`${hasSequence ? "Edit" : "Set up"} checkpoint sequence`}><IconButton
+                            disabled={checkpointLoading} onClick={() => {
                                 setSequenceEditorOpen(true);
                             }}><Schedule /></IconButton></Tooltip>
                     </div>
+
+                    <hr style={{ width: "100%" }} />
+
+
+                    {checkpointEditOpen ? <>
+
+                        <Alert severity="warning" style={{ marginBottom: '10px', fontSize: '.7em' }}>
+                            <AlertTitle style={{ fontSize: "1.2em" }}>Changing your primary model will affect all pending images</AlertTitle>
+                            Be careful if executing this while there's images brewing!
+                        </Alert>
+
+                        {hasSequence && <Alert severity="info" style={{ marginBottom: '10px', fontSize: '.7em' }}>
+                            <AlertTitle style={{ fontSize: "1.2em" }}>Changing your primary model will clear your sequence</AlertTitle>
+                            You can export it and import it again later on the sequence editor
+                        </Alert>}
+
+                        <div>
+                            <ModelSelector
+                                model={currentModelApi.data?.model} setModel={onChangeModel}
+                                disabled={changeModelApi.loading} loading={changeModelApi.loading}
+                                style={{ marginTop: '5px', marginBottom: '10px', flex: '1' }}
+                            />
+                        </div>
+                    </> : currentModelApi.loading || currentSequenceApi.loading ? <>
+                        <div style={{ height: "56px", padding: "10px", display: "flex", justifyContent: 'center' }}>
+                            <CircularProgress size={36} />
+                        </div>
+                    </> : <div style={{ marginBottom: '10px', display: 'flex', flexDirection: 'column', gap: '10px' }}>
+                        <ModelCard modelTitle={currentModelApi.data?.model} />
+                        {additionalModels.map(m => <ModelCard key={m.modelTitle} modelTitle={m.modelTitle} />)}
+                    </div>
+                    }
 
                     <ModelSequenceEditor
                         open={sequenceEditorOpen}
@@ -139,7 +166,7 @@ export default function PromptModelSelectorModal(props: {
                 </Alert> : <></>
             }
 
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: "100%" }}>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: "100%", marginTop: "5px" }}>
                 <div><b>Loras</b></div>
                 <Tooltip title={addOpen ? 'Cancel' : 'Add a LoRA'}><IconButton onClick={() => setAddOpen(!addOpen)}><Add sx={{ rotate: addOpen ? '45deg' : '' }} /></IconButton></Tooltip>
             </div>
