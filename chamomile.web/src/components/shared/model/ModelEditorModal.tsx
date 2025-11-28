5575c285d8b387f2ecdcc75475c97f998d057f84
GooeyAvocado <the.gooey.avocado@gmail.com>
2025-11-28 13:20:38 -0500

    Fix this quick bug

--- chamomile.web/src/components/shared/model/ModelEditorModal.tsx
+++ chamomile.web/src/components/shared/model/ModelEditorModal.tsx
@@ -106,8 +106,10 @@ export default function ModelEditorModal(props: {
         <ImageBrowserModal open={imageBrowserOpen} setOpen={setImageBrowserOpen} onOk={(a) => {
             setInternalModel({ ...internalModel, bannerImage: a.id })
             setImageBrowserOpen(false)
-        }} initialFilter={{
+        }} initialFilter={modelType === "Checkpoint" ? {
             model: model.id
+        } : {
+            lora: model.id
         }} />
     </Dialog>
 
