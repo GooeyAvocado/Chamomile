d50cf5e4e642fdcb9eb720fda6872fba4d8d561b
GooeyAvocado <the.gooey.avocado@gmail.com>
2025-12-01 17:26:20 -0500

    We are migrating back from LO because damn that was awful thanks GPT

--- Chamomile.Data/DDLs/LargeObjectMigrationScript.sql
+++ Chamomile.Data/DDLs/LargeObjectMigrationScript.sql
@@ -1,75 +1,42 @@
 ﻿-- THIS IS CRITICAL AS PART OF THIS UPDATE
 -- THIS WILL IMPROVE PERFORMANCE WHEN DEALING WITH LARGE NUMBERS OF IMAGES
 
-ALTER TABLE images ADD COLUMN image_lo_oid OID;
-ALTER TABLE images ADD COLUMN image_hires_lo_oid OID;
+CREATE TABLE ImageBinary_SD (
+    image_id int4 PRIMARY KEY,
+    image_bytes_tx BYTEA NOT NULL,
+    CONSTRAINT fk_sd_image
+        FOREIGN KEY (image_id) REFERENCES Images(image_id)
+        ON DELETE CASCADE
+);
 
-CREATE OR REPLACE FUNCTION bytea_to_lo(data BYTEA)
-RETURNS OID
-AS $$
-DECLARE
-    lo_id OID;
-BEGIN
-    lo_id := lo_create(0);                       -- Create new LO
-    PERFORM lowrite(lo_open(lo_id, 131072), data); -- 131072 = INV_WRITE
-    RETURN lo_id;
-END;
-$$ LANGUAGE plpgsql;
+CREATE TABLE ImageBinary_HD (
+    image_id int4 PRIMARY KEY,
+    image_bytes_tx BYTEA NOT NULL,
+    CONSTRAINT fk_sd_image
+        FOREIGN KEY (image_id) REFERENCES Images(image_id)
+        ON DELETE CASCADE
+);
 
-UPDATE images
-SET image_lo_oid = bytea_to_lo(image_bytes_tx)
-WHERE image_lo_oid IS NULL;
+INSERT INTO ImageBinary_SD (image_id, image_bytes_tx)
+SELECT image_id, image_bytes_tx
+FROM Images
+ON CONFLICT (image_id) DO NOTHING;
 
-UPDATE images
-SET image_hires_lo_oid = bytea_to_lo(image_hires_bytes_tx)
-WHERE image_hires_lo_oid IS null and image_hires_bytes_tx is not null;
-
---Execute this as  the SAME USER your Chamomile instance uses. If not, run this:
---Replace "to chamomile" with whatever your user is for chamomile
-
- CREATE OR REPLACE FUNCTION grant_on_hires_loids()
-RETURNS void AS $$
-DECLARE
-    lo_oid oid;
-BEGIN
-    FOR lo_oid IN
-        SELECT image_hires_lo_oid FROM images WHERE image_hires_lo_oid IS NOT NULL
-    LOOP
-        EXECUTE format(
-            'GRANT all ON LARGE OBJECT %s TO chamomile;',
-            lo_oid
-        );
-    END LOOP;
-END;
-$$ LANGUAGE plpgsql;
-
-
- CREATE OR REPLACE FUNCTION grant_on_sd_loids()
-RETURNS void AS $$
-DECLARE
-    lo_oid oid;
-BEGIN
-    FOR lo_oid IN
-        SELECT image_lo_oid FROM images WHERE image_lo_oid IS NOT NULL
-    LOOP
-        EXECUTE format(
-            'GRANT all ON LARGE OBJECT %s TO chamomile;',
-            lo_oid
-        );
-    END LOOP;
-END;
-$$ LANGUAGE plpgsql;
+INSERT INTO ImageBinary_HD (image_id, image_bytes_tx)
+SELECT image_id, image_hires_bytes_tx
+FROM Images
+WHERE image_hires_bytes_tx IS NOT NULL
+ON CONFLICT (image_id) DO NOTHING;
 
 --VERIFY that Chamomile is now able to access your images
 
 --Then:
 
 ALTER TABLE images
-    DROP COLUMN image_hires_in,
-    ADD COLUMN image_hires_in boolean GENERATED ALWAYS AS (image_hires_lo_oid IS NOT NULL) STORED;
+    DROP COLUMN image_hires_in;
 
 ALTER TABLE images
-ALTER COLUMN image_bytes_tx DROP NOT NULL;
+    ALTER COLUMN image_bytes_tx DROP NOT NULL;
 
 --VERIFY: That chamomile is able to CREATE, READ, UPSCALE, and DELETE images
 --MAKE SURE THIS WORKS. THIS IS THE POINT OF NO RETURN.
@@ -78,5 +45,5 @@ alter table images
 	drop column image_effective_size_nb;
 
 ALTER TABLE images
-drop COLUMN image_bytes_tx
-drop COLUMN image_hires_bytes_tx;
+    drop COLUMN image_bytes_tx
+    drop COLUMN image_hires_bytes_tx;
