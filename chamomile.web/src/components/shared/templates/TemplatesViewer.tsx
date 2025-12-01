0904a962db026cab0549f7288eb9a05366504294
GooeyAvocado <the.gooey.avocado@gmail.com>
2025-12-01 16:01:18 -0500

    Context Menu for Templates including Edit, Clone, and Delete

--- chamomile.web/src/components/shared/templates/TemplatesViewer.tsx
+++ chamomile.web/src/components/shared/templates/TemplatesViewer.tsx
@@ -1,28 +1,35 @@
-import { Assignment, Search } from "@mui/icons-material"
+import { Assignment, ContentCopy, Delete, Edit, Search } from "@mui/icons-material"
 import { Button, Card, CardActionArea, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, InputAdornment, TextField, Typography } from "@mui/material"
 import useApi from "../../hooks/useApi"
-import { getTemplates } from "../../../api/Template"
+import { deleteTemplate, getTemplates } from "../../../api/Template"
 import Template from "../../../model/Template"
 import { useEffect, useState } from "react"
 import { imageUrl } from "../../../api/Images"
 import TemplateEditor from "./TemplateEditor"
+import ContextMenu from "../ContextMenu"
+import { useSnackbar } from "notistack"
+import AreYouSureModal from "../modals/AreYouSureModal"
 
 export default function TemplatesViewer({ open, onClose }: {
     open: boolean,
     onClose: (dirty: boolean) => void
 }) {
 
+    const { enqueueSnackbar } = useSnackbar();
+
     const [filter, setFilter] = useState("")
     const [dirty, setDirty] = useState(false)
     const [editTempalte, setEditTemplate] = useState<Template>()
     const [templateEditorMode, setTemplateEditorMode] = useState<"create" | "edit">("create")
     const [templateEditorOpen, setTemplateEditorOpen] = useState(false)
+    const [deleteAys, setDeleteAys] = useState<Template>()
 
     useEffect(() => {
         if (open) { setDirty(false) }
     }, [open])
 
     const templatesApi = useApi(getTemplates, true)
+    const deleteApi = useApi(deleteTemplate);
 
     return <>
         <Dialog open={open} onClose={() => onClose(dirty)} maxWidth="sm" fullWidth>
@@ -50,22 +57,6 @@ export default function TemplatesViewer({ open, onClose }: {
                             <div>Loading templates...</div>
                         </div>
                         : <div style={{ flex: 1, overflowY: 'auto', display: 'flex', flexDirection: 'column', gap: "5px" }}>
-                            {templatesApi.data?.filter(
-                                filter.trim().length === 0 ? (_) => true :
-                                    t =>
-                                        t.name.toLowerCase().includes(filter.toLowerCase()) ||
-                                        t.description?.toLowerCase().includes(filter.toLowerCase()) ||
-                                        t.templateString.toLowerCase().includes(filter.toLowerCase())
-                            ).map(a => <TemplateCard
-                                key={a.name}
-                                template={a}
-                                onClick={() => {
-                                    //make a copy
-                                    setEditTemplate({ ...a })
-                                    setTemplateEditorMode("edit")
-                                    setTemplateEditorOpen(true)
-                                }}
-                            />)}
                             <TemplateCard
                                 template={{ name: "Create a new template", description: "Create a new template from scratch", templateString: "", params: [] } as Template}
                                 onClick={() => {
@@ -75,6 +66,52 @@ export default function TemplatesViewer({ open, onClose }: {
                                 }}
 
                             />
+                            {templatesApi.data?.filter(
+                                filter.trim().length === 0 ? (_) => true :
+                                    t =>
+                                        t.name.toLowerCase().includes(filter.toLowerCase()) ||
+                                        t.description?.toLowerCase().includes(filter.toLowerCase()) ||
+                                        t.templateString.toLowerCase().includes(filter.toLowerCase())
+                            ).map(a => <ContextMenu options={[
+                                {
+                                    icon: <Edit />,
+                                    text: "Edit",
+                                    onClick: () => {
+                                        setEditTemplate({ ...a })
+                                        setTemplateEditorMode("edit")
+                                        setTemplateEditorOpen(true)
+                                    }
+                                },
+                                {
+                                    icon: <ContentCopy />,
+                                    text: "Duplicate",
+                                    onClick: () => {
+                                        setTemplateEditorOpen(true);
+                                        setTemplateEditorMode("create")
+                                        setEditTemplate({ ...a })
+                                    }
+                                },
+                                {
+                                    type: "divider"
+                                },
+                                {
+                                    icon: <Delete />,
+                                    text: "Delete",
+                                    onClick: () => setDeleteAys(a)
+                                }
+                            ]}>
+                                <TemplateCard
+                                    key={a.name}
+                                    template={a}
+                                    onClick={() => {
+                                        //make a copy
+                                        setEditTemplate({ ...a })
+                                        setTemplateEditorMode("edit")
+                                        setTemplateEditorOpen(true)
+                                    }}
+                                />
+                            </ContextMenu>)}
+
                         </div>}
 
                 </div>
@@ -84,6 +121,20 @@ export default function TemplatesViewer({ open, onClose }: {
                 <Button onClick={() => onClose(dirty)}>OK</Button>
             </DialogActions>
         </Dialog>
+
+        <AreYouSureModal open={!!deleteAys} setOpen={() => setDeleteAys(undefined)} onYes={() => {
+            deleteApi.fetch(() => {
+                enqueueSnackbar("Template deleted!", { variant: 'success' })
+                setDeleteAys(undefined)
+                setDirty(true);
+                templatesApi.fetch();
+            }, (error) => {
+                console.error(error)
+                enqueueSnackbar("Template could not deleted", { variant: 'error' })
+            }, deleteAys?.name)
+        }}>
+            Are you sure you want to delete this template?
+        </AreYouSureModal>
 
         <TemplateEditor
             existingTemplates={templatesApi.data ?? []}
