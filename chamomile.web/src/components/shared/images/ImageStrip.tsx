bb5a09dbcdba7d089d667d466fafa05443a7667a
GooeyAvocado <the.gooey.avocado@gmail.com>
2025-12-01 21:20:29 -0500

    Size and layout for ImageStrip

--- chamomile.web/src/components/shared/images/ImageStrip.tsx
+++ chamomile.web/src/components/shared/images/ImageStrip.tsx
@@ -1,9 +1,11 @@
 import { Card } from "@mui/material";
 import { imageUrl } from "../../../api/Images";
 
-export default function ImageStrip({ images, maxLength }: {
+export default function ImageStrip({ images, maxLength, layout, imageSize }: {
     images: (number | undefined)[]
     maxLength?: number
+    layout?: "HORIZONTAL" | "VERTICAL"
+    imageSize?: string
 }) {
 
     //get the images that are not undefined
@@ -15,11 +17,12 @@ export default function ImageStrip({ images, maxLength }: {
         : definedImages;
 
     const plus = (definedImages.length - displayImages.length) + undefinedImageCount;
+    const size = imageSize ?? "32px"
 
-    return <div style={{ display: 'flex', gap: "5px" }}>
+    return <div style={{ display: 'flex', gap: "5px", flexDirection: layout === "VERTICAL" ? "column" : undefined }}>
         {displayImages.map(a => <Card key={a}>
-            <div style={{ maxWidth: "32px", aspectRatio: 1 / 1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', position: "relative" }}>
-                <img loading="lazy" src={imageUrl(a)} style={{ width: "100%", height: "100%", objectFit: 'cover', objectPosition: 'center top', position: 'absolute', left: '0', top: '0' }} />
+            <div style={{ width: size, aspectRatio: 1 / 1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', position: "relative" }}>
+                <img src={imageUrl(a)} style={{ width: "100%", height: "100%", objectFit: 'cover', objectPosition: 'center top', position: 'absolute', left: '0', top: '0' }} />
                 <img
                     src={imageUrl(a)}
                     style={{ width: "50%" }}
@@ -27,7 +30,7 @@ export default function ImageStrip({ images, maxLength }: {
             </div>
         </Card>)}
         {plus > 0 && <Card>
-            <div style={{ width: "32px", aspectRatio: 1 / 1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', fontSize: ".8em" }}>
+            <div style={{ width: size, aspectRatio: 1 / 1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', fontSize: ".8em" }}>
                 <div>+{plus}</div>
             </div>
         </Card>}
