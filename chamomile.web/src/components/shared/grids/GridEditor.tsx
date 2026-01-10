7e74c4affd78234b56628d52a2d34329b6e6295d
GooeyAvocado <the.gooey.avocado@gmail.com>
2026-01-10 17:37:03 -0500

    Reworked Grid Editor modal

--- chamomile.web/src/components/shared/grids/GridEditor.tsx
+++ chamomile.web/src/components/shared/grids/GridEditor.tsx
@@ -1,13 +1,15 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, IconButton, MenuItem, Select, TextField, Tooltip } from "@mui/material";
+import { Button, Card, FormControl, IconButton, MenuItem, Select, TextField, Tooltip } from "@mui/material";
 import { Grid } from "../../../model/Grid";
-import ComplexAccordion from "../complexAccordion/ComplexAccordion";
-import ComplexAccordionBody from "../complexAccordion/ComplexAccordionBody";
 import PromptBuilder from "../prompt/PromptBuilder";
 import { Add, Close, CompareArrows, OpenWith, Remove } from "@mui/icons-material";
 import { GridTypes } from "./GridTypes";
 import SamplerSelector from "../prompt/SamplerSelector";
 import SchedulerSelector from "../prompt/SchedulerSelector";
 import ModelSelector from "../checkpoint/CheckpointSelector";
+import TabbedModal from "../modals/TabbedModal/TabbedModal";
+import TabbedModalTabContent from "../modals/TabbedModal/TabbedModalTabContent";
+import TabbedModalActions from "../modals/TabbedModal/TabbedModalActions";
+import TabbedModalTitle from "../modals/TabbedModal/TabbedModalTitle";
 
 export default function GridEditor({ grid, setGrid, open, setOpen, onOk, loading, generated, duplicate }: {
     open: boolean,
@@ -27,56 +29,58 @@ export default function GridEditor({ grid, setGrid, open, setOpen, onOk, loading
         setGrid({ ...grid, xValMode: grid.yValMode, xVals: grid.yVals, yValMode: grid.xValMode, yVals: grid.xVals })
     }
 
-    return <Dialog open={open} maxWidth="md" fullWidth>
-        <DialogTitle>{duplicate ? "Duplicating existing grid" : editing ? "Editing Grid" : "Create a new Grid"}</DialogTitle>
-        <DialogContent style={{ display: "flex", flexDirection: 'column', gap: "10px", overflowY: "unset" }}>
+    return <TabbedModal open={open} setOpen={() => { }} maxWidth="md" fullWidth tabContentStyle={{ display: "flex", flexDirection: 'column', gap: "10px", overflowY: "auto", maxHeight: "75vh", minHeight: "50vh" }}>
+        <TabbedModalTitle>{duplicate ? "Duplicating existing grid" : editing ? "Editing Grid" : "Create a new Grid"}</TabbedModalTitle>
+        <TabbedModalTabContent label="Metadata">
             <TextField
-                label="Name" value={grid.name} fullWidth
+                label="Name" value={grid.name} fullWidth style={{ marginTop: "5px" }}
                 onChange={(e) => setGrid({ ...grid, name: e.target.value })}
             />
             <TextField fullWidth
                 label="Notes" value={grid.notes} multiline minRows={3}
                 onChange={(e) => setGrid({ ...grid, notes: e.target.value })}
             />
-            <ComplexAccordion title="Prompt">
-                <ComplexAccordionBody>
-                    <PromptBuilder
-                        alwaysExpand fullHeight noBrew prompt={{ ...grid, positivePrompt: grid.prompt, variables: {} }}
-                        setPrompt={(p) => setGrid({ ...grid, ...p, prompt: p.positivePrompt })} preview={(generated && !duplicate) || loading}
-                    />
-                </ComplexAccordionBody>
-            </ComplexAccordion>
-            <ComplexAccordion title="Values">
-                <ComplexAccordionBody style={{ display: 'flex', gap: "10px" }}>
-                    <div style={{ flex: "1" }}>
-                        <GridValsEditor axis="X" mode={grid.xValMode} vals={grid.xVals} disabled={(generated && !duplicate) || !!loading}
-                            setVals={a => setGrid({ ...grid, xVals: a })}
-                            setMode={a => setGrid({ ...grid, xValMode: a })}
-                        />
-                    </div>
-                    <div style={{ alignSelf: 'center' }}>
-                        <Tooltip title="Flip Axes">
-                            <IconButton onClick={() => flipAxes()} disabled={(generated && !duplicate) || !!loading}>
-                                <CompareArrows />
-                            </IconButton>
-                        </Tooltip>
+            <Card elevation={3} style={{ flexShrink: 0, padding: '20px' }}>
+                <PromptBuilder
+                    alwaysExpand fullHeight noBrew prompt={{ ...grid, positivePrompt: grid.prompt, variables: {} }}
+                    setPrompt={(p) => setGrid({ ...grid, ...p, prompt: p.positivePrompt })} preview={(generated && !duplicate) || loading}
+                />
+            </Card>
+        </TabbedModalTabContent>
+        <TabbedModalTabContent label="Columns and Rows" style={{ display: 'flex', gap: "10px" }}>
+            <div style={{ flex: "1" }}>
+                <GridValsEditor axis="X" mode={grid.xValMode} vals={grid.xVals} disabled={(generated && !duplicate) || !!loading}
+                    setVals={a => setGrid({ ...grid, xVals: a })}
+                    setMode={a => setGrid({ ...grid, xValMode: a })}
+                />
+            </div>
+            <div style={{ alignSelf: 'center' }}>
+                <Tooltip title="Flip Axes">
+                    <IconButton onClick={() => flipAxes()} disabled={(generated && !duplicate) || !!loading}>
+                        <CompareArrows />
+                    </IconButton>
+                </Tooltip>
+            </div>
+            <div style={{ flex: "1" }}>
+                <GridValsEditor axis="Y" mode={grid.yValMode} vals={grid.yVals} disabled={(generated && !duplicate) || !!loading}
+                    setVals={a => setGrid({ ...grid, yVals: a })}
+                    setMode={a => setGrid({ ...grid, yValMode: a })}
+                />
+            </div>
+        </TabbedModalTabContent>
+        <TabbedModalActions>
+            <div style={{ display: 'flex', justifyContent: "space-between", width: "100%", alignItems: 'center' }}>
+                <div style={{ marginLeft: "10px", fontSize: ".8em", opacity: ".8" }}>
+                    {grid.xVals.length} x {grid.yVals.length} ({grid.xVals.length * grid.yVals.length} images)
+                </div>
+                <div style={{ display: "flex", gap: "10px" }}>
+                    <Button disabled={loading} onClick={() => setOpen(false)}>Cancel</Button>
+                    <Button disabled={loading} onClick={onOk}>OK</Button>
+                </div>
+            </div>
+        </TabbedModalActions>
 
-                    </div>
-                    <div style={{ flex: "1" }}>
-                        <GridValsEditor axis="Y" mode={grid.yValMode} vals={grid.yVals} disabled={(generated && !duplicate) || !!loading}
-                            setVals={a => setGrid({ ...grid, yVals: a })}
-                            setMode={a => setGrid({ ...grid, yValMode: a })}
-                        />
-                    </div>
-                </ComplexAccordionBody>
-            </ComplexAccordion>
-        </DialogContent>
-        <DialogActions>
-            <Button disabled={loading} onClick={() => setOpen(false)}>Cancel</Button>
-            <Button disabled={loading} onClick={onOk}>OK</Button>
-        </DialogActions>
-
-    </Dialog>
+    </TabbedModal>
 
 }
 
@@ -105,7 +109,7 @@ function GridValsEditor({
 
     const type = GridTypes.find(a => a.code === mode)
 
-    return <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+    return <Card style={{ display: "flex", flexDirection: "column", gap: "10px", padding: "10px" }}>
         <FormControl fullWidth >
             <Select
                 disabled={disabled}
@@ -116,61 +120,74 @@ function GridValsEditor({
             </Select>
         </FormControl>
 
-        {type && type.type !== "none" && <>
-            {/* <hr style={{ width: "100%" }} /> */}
-            {vals.map((a, i) => <div style={{ display: 'flex', gap: "10px", alignItems: 'center' }}>
-                <IconButton onClick={() => setVals(removeFromArray(vals, i))} disabled={disabled}><Remove /></IconButton>
-                {
-                    ["float", "int", "string", "multiline"].includes(type.type) ? <TextField fullWidth
-                        value={a} type={["float", "int"].includes(type.type) ? "number" : undefined}
-                        multiline={type.type === "multiline"}
-                        slotProps={{
-                            htmlInput: {
-                                step: type.type === "float" ? 0.1 : type.type === "int" ? 1 : undefined,
-                                min: ["float", "int"].includes(type.type) ? 0 : undefined
-                            }
-                        }} disabled={disabled}
-                        onChange={(e) => setVals(updateInArray(vals, i, e.target.value))}
-                    /> : type.type === "sampler" ? <SamplerSelector sampler={a} setSampler={a => setVals(updateInArray(vals, i, a))} disabled={disabled} />
-                        : type.type === "scheduler" ? <SchedulerSelector scheduler={a} setScheduler={a => setVals(updateInArray(vals, i, a))} disabled={disabled} />
-                            : type.type === "model" ? <ModelSelector model={a} setModel={a => setVals(updateInArray(vals, i, a.id))} disabled={disabled} style={{ width: "100%" }} />
-                                : type.type === "dimensions" ?
-                                    <div style={{
-                                        flex: "1", alignItems: 'center', borderRadius: "4px",
-                                        border: "1px solid rgba(255,255,255, 0.23)", minWidth: "260px"
-                                    }}>
+        <hr style={{ width: "100%" }} />
 
-                                        {/* This is necessary. If this isn't in a sub-div, the padding adds to the width and makes flex:1 not be half and half */}
-                                        {/* border-box does not solve this issue either. I have no idea why, but hey, this works. So we're good */}
-                                        <div style={{ display: 'flex', gap: "10px", width: "100%", height: "56px", alignItems: 'center', padding: "0 16px" }}>
-                                            <OpenWith sx={{ margin: "-7px", marginRight: "5px" }} />
+        <div style={{ height: "50vh", overflowY: "auto", display: 'flex', flexDirection: "column", gap: "10px" }}>
+            {type && type.type !== "none" ? <>
 
-                                            {/* Width */}
-                                            <TextField type="number" disabled={disabled}
-                                                value={a.split('x')[0]} onChange={(e) => setVals(updateInArray(vals, i, [e.target.value, a.split('x')[1] ?? ""].join("x")))}
-                                                placeholder="1024" fullWidth slotProps={{ htmlInput: { min: 1 }, }} variant="standard"
-                                                style={{ flex: "1", minWidth: "45px" }}
-                                                size="small"
-                                            />
-                                            <Close fontSize="inherit" />
-                                            {/* Height */}
-                                            <TextField type="number" disabled={disabled}
-                                                value={a.split('x')[1] ?? ""} onChange={(e) => setVals(updateInArray(vals, i, [a.split('x')[0] ?? "", e.target.value].join("x")))}
-                                                placeholder="1024" fullWidth slotProps={{ htmlInput: { min: 1 }, }} variant="standard"
-                                                style={{ flex: "1", minWidth: "45px" }}
-                                                size="small"
-                                            />
-                                            <div>px</div>
+                {vals.map((a, i) => <div style={{ display: 'flex', gap: "10px", alignItems: 'center' }}>
+                    <IconButton onClick={() => setVals(removeFromArray(vals, i))} disabled={disabled}><Remove /></IconButton>
+                    {
+                        ["float", "int", "string", "multiline"].includes(type.type) ? <TextField fullWidth
+                            value={a} type={["float", "int"].includes(type.type) ? "number" : undefined}
+                            multiline={type.type === "multiline"}
+                            slotProps={{
+                                htmlInput: {
+                                    step: type.type === "float" ? 0.1 : type.type === "int" ? 1 : undefined,
+                                    min: ["float", "int"].includes(type.type) ? 0 : undefined
+                                }
+                            }} disabled={disabled}
+                            onChange={(e) => setVals(updateInArray(vals, i, e.target.value))}
+                        /> : type.type === "sampler" ? <SamplerSelector sampler={a} setSampler={a => setVals(updateInArray(vals, i, a))} disabled={disabled} />
+                            : type.type === "scheduler" ? <SchedulerSelector scheduler={a} setScheduler={a => setVals(updateInArray(vals, i, a))} disabled={disabled} />
+                                : type.type === "model" ? <ModelSelector model={a} setModel={a => setVals(updateInArray(vals, i, a.id))} disabled={disabled} style={{ width: "100%" }} />
+                                    : type.type === "dimensions" ?
+                                        <div style={{
+                                            flex: "1", alignItems: 'center', borderRadius: "4px",
+                                            border: "1px solid rgba(255,255,255, 0.23)", minWidth: "260px"
+                                        }}>
+
+                                            {/* This is necessary. If this isn't in a sub-div, the padding adds to the width and makes flex:1 not be half and half */}
+                                            {/* border-box does not solve this issue either. I have no idea why, but hey, this works. So we're good */}
+                                            <div style={{ display: 'flex', gap: "10px", width: "100%", height: "56px", alignItems: 'center', padding: "0 16px" }}>
+                                                <OpenWith sx={{ margin: "-7px", marginRight: "5px" }} />
+
+                                                {/* Width */}
+                                                <TextField type="number" disabled={disabled}
+                                                    value={a.split('x')[0]} onChange={(e) => setVals(updateInArray(vals, i, [e.target.value, a.split('x')[1] ?? ""].join("x")))}
+                                                    placeholder="1024" fullWidth slotProps={{ htmlInput: { min: 1 }, }} variant="standard"
+                                                    style={{ flex: "1", minWidth: "45px" }}
+                                                    size="small"
+                                                />
+                                                <Close fontSize="inherit" />
+                                                {/* Height */}
+                                                <TextField type="number" disabled={disabled}
+                                                    value={a.split('x')[1] ?? ""} onChange={(e) => setVals(updateInArray(vals, i, [a.split('x')[0] ?? "", e.target.value].join("x")))}
+                                                    placeholder="1024" fullWidth slotProps={{ htmlInput: { min: 1 }, }} variant="standard"
+                                                    style={{ flex: "1", minWidth: "45px" }}
+                                                    size="small"
+                                                />
+                                                <div>px</div>
 
 
+                                            </div>
                                         </div>
-                                    </div>
-                                    : <></>
-                }
-            </div>)}
-            <Button style={{ marginTop: "10px" }} startIcon={<Add />} disabled={disabled} onClick={() => setVals(addToArray(vals, ""))}>Add new {axis === "Y" ? "row" : "column"}</Button>
-        </>}
-    </div>
+                                        : <></>
+                    }
+                </div>)}
+
+
+            </> : <div style={{ textAlign: 'center' }}>Select a type to get started</div>}
+        </div>
+
+        <hr style={{ width: "100%" }} />
+        <Button
+            startIcon={<Add />}
+            disabled={disabled || !type || type.type === "none"}
+            onClick={() => setVals(addToArray(vals, ""))}>
+            Add new {axis === "Y" ? "row" : "column"}
+        </Button>
+    </Card>
 
 
 }
