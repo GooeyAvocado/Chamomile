6543f1e27122d164a87d085b84e5bcdbfea2c13d
GooeyAvocado <the.gooey.avocado@gmail.com>
2026-07-08 20:49:13 -0400

    Fix Album Thumb Image bug for empty albums

--- chamomile.web/src/components/shared/albums/AlbumThumbImg.tsx
+++ chamomile.web/src/components/shared/albums/AlbumThumbImg.tsx
@@ -14,7 +14,9 @@ export default function AlbumThumbImg({ album, defaultImage }: {
                 ? <img src={imageUrl(album.thumbId)} style={{ width: "100%", height: "100%", aspectRatio: "2/1", objectFit: 'cover', objectPosition: 'center top', position: 'absolute', left: '0', top: '0' }} />
                 : <div style={{ display: 'flex', flexWrap: "wrap", width: "100%", height: "100%", aspectRatio: "2/1", objectFit: 'cover', objectPosition: 'center top', position: 'absolute', left: '0', top: '0' }}>
                     {(album.firstFourImages?.length ?? 0) < 3
-                        ? <img src={imageUrl(album.firstFourImages?.[0])} style={{ width: "100%", height: "100%", aspectRatio: "2/1", objectFit: 'cover', objectPosition: 'center top', position: 'absolute', left: '0', top: '0' }} />
+                        ? (album.firstFourImages?.length ?? 0) > 0
+                            ? <img src={imageUrl(album.firstFourImages?.[0])} style={{ width: "100%", height: "100%", aspectRatio: "2/1", objectFit: 'cover', objectPosition: 'center top', position: 'absolute', left: '0', top: '0' }} />
+                            : <></>
                         : album.firstFourImages?.map((a) =>
                             <img key={`albumsample-${a}`} src={imageUrl(a)} style={{ flex: '1', width: "100%", height: "50%", aspectRatio: "2/1", objectFit: "cover", objectPosition: 'center top' }} />
                         )}
