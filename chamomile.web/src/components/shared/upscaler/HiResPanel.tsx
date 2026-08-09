45d6f4be4dec13cfd27fff52ae9511440d12953e
GooeyAvocado <the.gooey.avocado@gmail.com>
2026-08-09 19:50:24 -0400

    Nicer upscale panel

--- chamomile.web/src/components/shared/upscaler/HiResPanel.tsx
+++ chamomile.web/src/components/shared/upscaler/HiResPanel.tsx
@@ -1,7 +1,8 @@
-import { Button, Card, CircularProgress, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
+import { Button, Card, CircularProgress, FormControl, InputLabel, MenuItem, Select, TextField, Tooltip } from "@mui/material";
 import { GeneratedImage } from "../../../model/GeneratedImage";
 import { useUpscalers } from "../../hooks/useUpscalers";
 import { usePingPong } from "../../hooks/usePingPong";
+import { Gradient } from "@mui/icons-material";
 
 export default function HiResPanel(props: {
     image?: GeneratedImage
@@ -19,32 +20,39 @@ export default function HiResPanel(props: {
     } = useUpscalers()
 
     return <Card elevation={7}>
-        <div style={{ padding: '10px', height: "100px", display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: upscaleLoading ? 'center' : '' }}>
+        <div style={{ padding: '0px 10px', height: "60px", display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: upscaleLoading ? 'center' : '' }}>
             {upscaleLoading ? <>
                 <CircularProgress />
             </> : <>
-                <div style={{ display: 'flex', gap: '10px', marginTop: '5px' }}>
+                <div style={{ display: 'flex', gap: '10px' }}>
 
-                    <FormControl fullWidth disabled={!pong?.SD}>
+                    <FormControl fullWidth disabled={!pong?.SD} style={{ flex: "1" }}>
                         <InputLabel>Upscaler</InputLabel>
                         <Select
                             value={selectedUpscaler} onChange={(e) => setSelectedUpscaler(e.target.value)} size="small" label='Upscaler'
-                            slotProps={{ root: { style: { fontSize: '.8em' } } }}
+                            slotProps={{ root: { style: { fontSize: '1em' } } }}
                         >
-                            {upscalers?.map((u) => <MenuItem key={u} value={u} style={{ fontSize: '.8em' }}>{u}</MenuItem>)}
+                            {upscalers?.map((u) => <MenuItem key={u} value={u}>{u}</MenuItem>)}
                         </Select>
                     </FormControl>
+
+
 
                     <TextField
                         size="small" variant="outlined" label='Scale' type="number"
                         value={upscaleScale} onChange={(e) => setUpscaleScale(Number.parseFloat(e.target.value))}
-                        slotProps={{ htmlInput: { step: '.5', style: { fontSize: '.8em' } } }} style={{ width: "100px" }}
+                        slotProps={{ htmlInput: { step: '.5', style: { fontSize: '1em', textAlign: "center" } } }} style={{ width: "56px" }}
                     />
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'end', marginTop: '5px', alignItems: 'center' }}>
-                    <Button size="small" onClick={() => {
-                        onUpscale(image ?? {} as GeneratedImage, updateImage)
-                    }} disabled={!updateImage || !pong?.SD || selectedUpscaler === "None"}>Upscale {image?.hiResAvailable ? "Again" : ""}</Button>
+
+                    <Tooltip title={image?.hiResAvailable ? "Re-Upscale" : "Upscale"}>
+                        <Button
+                            fullWidth style={{ width: "0px" }} variant="outlined"
+                            onClick={() => { onUpscale(image ?? {} as GeneratedImage, updateImage) }}
+                            disabled={!updateImage || !pong?.SD || selectedUpscaler === "None"}
+                        >
+                            <Gradient />
+                        </Button>
+                    </Tooltip>
                 </div>
             </>}
         </div>
